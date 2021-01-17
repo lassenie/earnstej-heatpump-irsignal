7589aab9bc3b5e728fe9fad26d89a78af66dc808
Lasse Nielsen <lasse.nielsen@bitzerdk.com>
2021-01-17 20:46:34 +0100

    Added temperature selection.

--- earnstej-heatpump-irsignal.ino
+++ earnstej-heatpump-irsignal.ino
@@ -3,7 +3,7 @@
 // Controlling a heat pump via IR (e.g. TSAL6400 LED). 
 
 #include <avr/wdt.h>
-#include <PanasonicHeatpumpIR.h>
+#include <PanasonicHeatpumpIR.h> // HeatPumpIR library by Toni Arte (https://github.com/ToniA/arduino-heatpumpir) - last tested with version 1.0.15
 
 #define IR_SENDER_PIN           3 // PWM pin
 #define FORCE_UPDATE_INPUT_PIN  5
@@ -11,12 +11,19 @@
 #define HEAT_INPUT_PIN1         7
 #define HEAT_INPUT_PIN2         8
 #define HEAT_INPUT_PIN3         9
+#define TEMP_SELECT_OUT_PIN     10
+#define TEMP_SELECT_IN1_PIN     11
+#define TEMP_SELECT_IN2_PIN     12
 #define LED_OUTPUT_PIN          13
 
-#define TEMPERATURE_SETPOINT       25
+#define TEMPERATURE_SETPOINT_0     22 // Pins not wired
+#define TEMPERATURE_SETPOINT_1     23 // TEMP_SELECT_OUT_PIN wired to TEMP_SELECT_IN1_PIN
+#define TEMPERATURE_SETPOINT_2     24 // TEMP_SELECT_OUT_PIN wired to TEMP_SELECT_IN2_PIN
+#define TEMPERATURE_SETPOINT_3     25 // TEMP_SELECT_OUT_PIN wired to TEMP_SELECT_IN1_PIN+TEMP_SELECT_IN2_PIN
 #define UPDATE_INTERVAL_MSEC      200
 #define STEADY_INPUT_STATE_MSEC  1000UL
-#define BOOT_DELAY_SECONDS         10
+#define BOOT_DELAY_SECONDS         10 // Must be at least 2 seconds because of watchdog disabling during startup
+#define REBOOT_INTERVAL_MSEC     (20 * 1000) // 700000000 // A little more than a week
 
 HeatpumpIR *pHeatpumpIR = new PanasonicNKEHeatpumpIR(); // NKE model has 8/10 degrees maintenance with max. fan speed //PanasonicDKEHeatpumpIR();
 
@@ -33,13 +40,17 @@ void setup()
   Serial.begin(9600);
   Serial.println(F("setup()"));
 
-  pinMode(HEAT_INPUT_PIN1,        INPUT_PULLUP);
-  pinMode(HEAT_INPUT_PIN2,        INPUT_PULLUP);
-  pinMode(HEAT_INPUT_PIN3,        INPUT_PULLUP);
-  pinMode(FROST_INPUT_PIN,        INPUT_PULLUP);
-  pinMode(FORCE_UPDATE_INPUT_PIN, INPUT_PULLUP);
-  pinMode(LED_OUTPUT_PIN,         OUTPUT);        
-  digitalWrite(LED_OUTPUT_PIN,    LOW);
+  pinMode(HEAT_INPUT_PIN1,          INPUT_PULLUP);
+  pinMode(HEAT_INPUT_PIN2,          INPUT_PULLUP);
+  pinMode(HEAT_INPUT_PIN3,          INPUT_PULLUP);
+  pinMode(FROST_INPUT_PIN,          INPUT_PULLUP);
+  pinMode(FORCE_UPDATE_INPUT_PIN,   INPUT_PULLUP);
+  pinMode(TEMP_SELECT_OUT_PIN,      OUTPUT);
+  pinMode(TEMP_SELECT_IN1_PIN,      INPUT_PULLUP);
+  pinMode(TEMP_SELECT_IN2_PIN,      INPUT_PULLUP);
+  pinMode(LED_OUTPUT_PIN,           OUTPUT);        
+  digitalWrite(LED_OUTPUT_PIN,      LOW);
+  digitalWrite(TEMP_SELECT_OUT_PIN, LOW);
 
   wdt_disable();  /* Disable the watchdog and wait for more than 2 seconds so that the Arduino doesn't keep resetting infinitely in case of wrong configuration */
 
@@ -61,6 +72,13 @@ void loop()
   wdt_reset(); /* Reset the watchdog */
 
   delay(UPDATE_INTERVAL_MSEC);
+
+  // Time to reboot at reqular interval?
+  if (millis() > REBOOT_INTERVAL_MSEC)
+  {
+    // Let the watchdog do the rebooting
+    while (true);
+  }
   
   if (checkUpdatedInputs())
     updateIR();
@@ -70,6 +88,7 @@ bool checkUpdatedInputs()
 {
   // Heat input considered activated (low) if at least one of the heat inputs is low
   int heatInputVal = ((digitalRead(HEAT_INPUT_PIN1) == LOW) || (digitalRead(HEAT_INPUT_PIN2) == LOW) || (digitalRead(HEAT_INPUT_PIN3) == LOW)) ? LOW : HIGH;
+
   int frostInputVal = digitalRead(FROST_INPUT_PIN);
   int updateInputVal = digitalRead(FORCE_UPDATE_INPUT_PIN);
 
@@ -111,7 +130,7 @@ void updateIR()
   Serial.println(F("updateIR()"));
 
   byte power = (heatInputState == LOW) || (frostInputState == LOW) ? POWER_ON : POWER_OFF;
-  byte temp  = (heatInputState == LOW) ? TEMPERATURE_SETPOINT : ((frostInputState == LOW) ? 8 : 0);
+  byte temp  = (heatInputState == LOW) ? getTemperatureSetpoint() : ((frostInputState == LOW) ? 8 : 0);
 
   Serial.print(F("Power: ")); Serial.println(power);
   Serial.print(F("Temp: ")); Serial.println(temp);
@@ -123,4 +142,20 @@ void updateIR()
   pHeatpumpIR->send(irSender, power, MODE_HEAT, FAN_AUTO, temp, VDIR_UP, HDIR_AUTO);
 
   digitalWrite(LED_OUTPUT_PIN, LOW);
+}
+
+byte getTemperatureSetpoint()
+{
+  switch (((digitalRead(TEMP_SELECT_IN2_PIN) == LOW) ? 2 : 0) + ((digitalRead(TEMP_SELECT_IN1_PIN) == LOW) ? 1 : 0))
+  {
+    case 3:
+      return TEMPERATURE_SETPOINT_3;
+    case 2:
+      return TEMPERATURE_SETPOINT_2;
+    case 1:
+      return TEMPERATURE_SETPOINT_1;
+    case 0:
+    default:
+      return TEMPERATURE_SETPOINT_0;
+  }
 }
