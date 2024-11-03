50e7df74a1d2dbca17410a6428d22b5302b81c1c
Lasse Nielsen <ln@orgnet.dk>
2024-11-03 18:38:26 +0100

    Heat signals now as pulse, heating some hours, then off.

--- README.md
+++ README.md
@@ -12,20 +12,20 @@ Krævede kodebiblioteker:
 Varmepumpen kan enten være:
 
 * Slukket
-* På sænket temperatur (8/10 grader, fuld ventilation - en indbygget funktion i varmepumpen).
-* Tændt på normal temperatur (se temperaturvalg nedenfor)
+* Tændt på normal temperatur (se temperaturvalg nedenfor) - slukker automatisk efter 24 timer
 
 Inputs:
 
-* Varmesignal, 3 stk. - hvis et af dem er lavt (0 Volt), tændes varmepumpen. Svævende indgange betragtes som høje (5 Volt via interne pull up-modstande). Varmesignaler kan f.eks. være:
-  * Tænd-knap som brugere kan aktivere for at få varme tændt.
-  * Automatisk signal, f.eks. fra timer der aktiverer et relæ som kobler indgang til Vcc (5 Volt).
-* Frost-signal - hvis dette er lavt (0 Volt), køres sænket temperature i stedet for at slukke varmepumpen når der ikke er varmesignal. Svævende indgang betragtes som høj (5 Volt via intern pull up-modstand). Frost-signal kan f.eks. være:
-  * Knap som stilles til mildt vejr eller frostvejr.
-* Opdateringssignal - hvis dette momentant - dvs. i 1 sekund - er lavt (0 Volt), sendes et ekstra IR-signal til varmepumpen (kan evt. bruges hvis IR-pumpen er blevet påvirket med fjernbetjening eller af anden årsag kører anderledes end forventet). Svævende indgang betragtes som høj (5 Volt via intern pull up-modstand). Dette signal bruges ikke nødvendigvis.
+* Varmesignal, 3 stk. - hvis et af dem er lavt (0 Volt), skiftes mellem slukket og tændt. Svævende indgange betragtes som høje (5 Volt via interne pull up-modstande). De tre indgange giver:
+  * 12 timer (nok til et møde)
+  * 24 timer (nok til et dags-/døgnarrangement)
+  * 72 timer (nok til en weekend)
+* Vertikal luftretning - hvis dette er lavt (0 Volt), køres høj luftretning, ellers lav luftretning.
+* Opdateringssignal - hvis dette momentant er lavt (0 Volt), sendes et ekstra IR-signal til varmepumpen (kan evt. bruges hvis IR-pumpen er blevet påvirket med fjernbetjening eller af anden årsag kører anderledes end forventet). Svævende indgang betragtes som høj (5 Volt via intern pull up-modstand). Dette signal bruges ikke nødvendigvis.
 * Temperaturvalg, 2 stk. vælger 22, 23, 24 eller 25 grader afhængigt af hvilke af de to inputs der er forbundet til temperaturvalgs-udgangen.
 
 Outputs:
 
 * IR-output - til at drive IR-diode som er koblet mellem digital udgang og GND via formodstand (se IR-diodens data for hvilken strøm der kræves - bemærk hvor mange milliampere der max. kan gives på udgangen).
+* LED-status - til at indikere aktivitet ved skift af indstilling eller opdatering af varmepumpe (IR-sending).
 * Temperaturvalg - konstant lavt (0 Volt) til at trække evt. forbundne temperaturvalgs-indgange ned.
--- earnstej-heatpump-irsignal.ino
+++ earnstej-heatpump-irsignal.ino
@@ -1,17 +1,17 @@
 // earnstej-heatpump-irsignal
-// Copyright (c) 2020 Lasse Nielsen
+// Copyright (c) 2020-2024 Lasse Nielsen
 // Controlling a heat pump via IR (e.g. TSAL6400 LED). 
 
 #include <avr/wdt.h>
 #include <PanasonicHeatpumpIR.h> // HeatPumpIR library by Toni Arte (https://github.com/ToniA/arduino-heatpumpir) - last tested with version 1.0.15
 
-#define IR_SENDER_PIN           3 // PWM pin
-#define EXT_LED_OUTPUT_PIN      4
-#define FORCE_UPDATE_INPUT_PIN  5
-#define FROST_INPUT_PIN         6
-#define HEAT_INPUT_PIN1         7
-#define HEAT_INPUT_PIN2         8
-#define HEAT_INPUT_PIN3         9
+#define IR_SENDER_PIN            3 // PWM pin
+#define EXT_LED_OUTPUT_PIN       4
+#define FORCE_UPDATE_INPUT_PIN   5
+#define VERTICAL_DIR_INPUT_PIN   6
+#define HEAT_TOGGLE_IN1_PIN      7
+#define HEAT_TOGGLE_IN2_PIN      8
+#define HEAT_TOGGLE_IN3_PIN      9
 #define TEMP_SELECT_OUT_PIN     10
 #define TEMP_SELECT_IN1_PIN     11
 #define TEMP_SELECT_IN2_PIN     12
@@ -23,9 +23,15 @@
 #define TEMPERATURE_SETPOINT_3     25 // TEMP_SELECT_OUT_PIN wired to TEMP_SELECT_IN1_PIN+TEMP_SELECT_IN2_PIN
 #define UPDATE_INTERVAL_MSEC      200
 #define IR_SIGNAL_PAUSE_BLINKS      3 // x 2 x UPDATE_INTERVAL_MSEC = pause duration
-#define STEADY_INPUT_STATE_MSEC  1000UL
+#define STEADY_INPUT_STATE_MSEC  100UL // Must be short enough to allow a user briefly pressing a button, but long enough to handle debounching
 #define BOOT_DELAY_SECONDS         10 // Must be at least 2 seconds because of watchdog disabling during startup
 //#define REBOOT_INTERVAL_MSEC     700000000UL // A little more than a week
+
+#define HEAT_TIME_MILLIS_1  (12 * 60 * 60 * 1000) // 12 hours - enough for a meeting
+#define HEAT_TIME_MILLIS_2  (24 * 60 * 60 * 1000) // 24 hours - enough for a one-day event
+#define HEAT_TIME_MILLIS_3  (72 * 60 * 60 * 1000) // 72 hours - enough for a weekend
+
+#define HEAT_MILLIS_MAX  HEAT_TIME_MILLIS_3 // The highest of the heat times
 
 HeatpumpIR *pHeatpumpIR = new PanasonicNKEHeatpumpIR(); // NKE model has 8/10 degrees maintenance with max. fan speed //PanasonicDKEHeatpumpIR();
 
@@ -33,9 +39,13 @@ IRSenderPWM irSender(IR_SENDER_PIN);  // IR led on Arduino, using Arduino PWM
 
 unsigned long lastStateChangeTick = 1;
 
+bool togglingHeatMode = false;
+
 int heatInputState = -1;
-int frostInputState = -1;
+int verticalDirInputState = -1;
 int updateInputState = -1;
+
+unsigned long heatOffMillis = 0; // Heat off
 
 void setup()
 {
@@ -44,10 +54,10 @@ void setup()
   Serial.begin(9600);
   Serial.println(F("setup()"));
 
-  pinMode(HEAT_INPUT_PIN1,          INPUT_PULLUP);
-  pinMode(HEAT_INPUT_PIN2,          INPUT_PULLUP);
-  pinMode(HEAT_INPUT_PIN3,          INPUT_PULLUP);
-  pinMode(FROST_INPUT_PIN,          INPUT_PULLUP);
+  pinMode(HEAT_TOGGLE_IN1_PIN,      INPUT_PULLUP);
+  pinMode(HEAT_TOGGLE_IN2_PIN,      INPUT_PULLUP);
+  pinMode(HEAT_TOGGLE_IN3_PIN,      INPUT_PULLUP);
+  pinMode(VERTICAL_DIR_INPUT_PIN,   INPUT_PULLUP);
   pinMode(FORCE_UPDATE_INPUT_PIN,   INPUT_PULLUP);
   pinMode(TEMP_SELECT_OUT_PIN,      OUTPUT);
   pinMode(TEMP_SELECT_IN1_PIN,      INPUT_PULLUP);
@@ -87,19 +97,22 @@ void loop()
 //  }
   
   if (checkUpdatedInputs())
+  {
+    toggleHeatModeIfRelevant();
     updateIR();
+  }
 }
 
 bool checkUpdatedInputs()
 {
   // Heat input considered activated (low) if at least one of the heat inputs is low
-  int heatInputVal = ((digitalRead(HEAT_INPUT_PIN1) == LOW) || (digitalRead(HEAT_INPUT_PIN2) == LOW) || (digitalRead(HEAT_INPUT_PIN3) == LOW)) ? LOW : HIGH;
+  int heatInputVal = ((digitalRead(HEAT_TOGGLE_IN1_PIN) == LOW) || (digitalRead(HEAT_TOGGLE_IN2_PIN) == LOW) || (digitalRead(HEAT_TOGGLE_IN3_PIN) == LOW)) ? LOW : HIGH;
 
-  int frostInputVal = digitalRead(FROST_INPUT_PIN);
+  int verticalDirInputVal = digitalRead(VERTICAL_DIR_INPUT_PIN);
   int updateInputVal = digitalRead(FORCE_UPDATE_INPUT_PIN);
 
   // Just changing input state?
-  if ((heatInputVal != heatInputState) || (frostInputVal != frostInputState) || (updateInputVal != updateInputState))
+  if ((heatInputVal != heatInputState) || (verticalDirInputVal != verticalDirInputState) || (updateInputVal != updateInputState))
   {
     lastStateChangeTick = millis();
 
@@ -110,8 +123,16 @@ bool checkUpdatedInputs()
     if (lastStateChangeTick == 0)
       lastStateChangeTick = 1;
 
+    // Seeing a change on the heat input?
+    if (heatInputVal != heatInputState)
+    {
+      // High-to-low, i.e. user pressing the button?
+      // If low-to-high we either see end of button press or bounching button
+      togglingHeatMode = (heatInputVal == LOW);
+    }
+
     heatInputState = heatInputVal;
-    frostInputState = frostInputVal;
+    verticalDirInputState = verticalDirInputVal;
     updateInputState = updateInputVal;
 
     // Not steady yet
@@ -135,21 +156,48 @@ bool checkUpdatedInputs()
   return false;
 }
 
+void toggleHeatModeIfRelevant()
+{
+    // Not toggling heat mode?
+    if (!togglingHeatMode || (heatInputState == HIGH))
+      return;
+
+    if (digitalRead(HEAT_TOGGLE_IN1_PIN) == LOW)
+    {
+      // Toggle heating interval 1
+      heatOffMillis = isHeating() ? 0 : millis() + HEAT_TIME_MILLIS_1;
+    }
+    else if (digitalRead(HEAT_TOGGLE_IN2_PIN) == LOW)
+    {
+      // Toggle heating interval 2
+      heatOffMillis = isHeating() ? 0 : millis() + HEAT_TIME_MILLIS_2;
+    }
+    else if (digitalRead(HEAT_TOGGLE_IN3_PIN) == LOW)
+    {
+      // Toggle heating interval 3
+      heatOffMillis = isHeating() ? 0 : millis() + HEAT_TIME_MILLIS_3;
+    }
+
+    togglingHeatMode = false;
+}
+
 void updateIR()
 {
   Serial.println(F("updateIR()"));
 
-  byte power = (isHeating() || isAntifrost()) ? POWER_ON : POWER_OFF;
-  byte temp  = isHeating() ? getTemperatureSetpoint() : (isAntifrost() ? 8 : 0);
+  byte power = isHeating() ? POWER_ON : POWER_OFF;
+  byte temp  = isHeating() ? getTemperatureSetpoint() : 0;
+  byte vertDir = getVerticalDirection();
 
   Serial.print(F("Power: ")); Serial.println(power);
   Serial.print(F("Temp: ")); Serial.println(temp);
+  Serial.print(F("VertDir: ")); Serial.println(vertDir);
 
-  pHeatpumpIR->send(irSender, power, MODE_HEAT, FAN_AUTO, temp, VDIR_UP, HDIR_AUTO);
+  pHeatpumpIR->send(irSender, power, MODE_HEAT, FAN_AUTO, temp, vertDir, HDIR_AUTO);
   pauseIRWhileBlinkingExtLED();
-  pHeatpumpIR->send(irSender, power, MODE_HEAT, FAN_AUTO, temp, VDIR_UP, HDIR_AUTO);
+  pHeatpumpIR->send(irSender, power, MODE_HEAT, FAN_AUTO, temp, vertDir, HDIR_AUTO);
   pauseIRWhileBlinkingExtLED();
-  pHeatpumpIR->send(irSender, power, MODE_HEAT, FAN_AUTO, temp, VDIR_UP, HDIR_AUTO);
+  pHeatpumpIR->send(irSender, power, MODE_HEAT, FAN_AUTO, temp, vertDir, HDIR_AUTO);
 
   digitalWrite(INT_LED_OUTPUT_PIN, LOW);
 
@@ -170,12 +218,23 @@ void pauseIRWhileBlinkingExtLED()
 
 bool isHeating()
 {
-  return (heatInputState == LOW);
+  // Heating?
+  if (heatOffMillis != 0)
+  {
+    unsigned long heatMillisLeft = heatOffMillis - millis();
+
+    // Overflow, i.e. no heating time left
+    if (heatMillisLeft > HEAT_MILLIS_MAX)
+      heatOffMillis = 0; // Turn off heating
+  }
+
+  // Heating?
+  return (heatOffMillis != 0);
 }
 
-bool isAntifrost()
+bool getVerticalDirection()
 {
-  return (frostInputState == LOW);
+  return (verticalDirInputState == LOW) ? VDIR_UP : VDIR_DOWN;
 }
 
 byte getTemperatureSetpoint()
